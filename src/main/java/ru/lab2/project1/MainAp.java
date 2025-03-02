45f1ef12dcfdb52ee53f449a1e08bb65ad15e6f0
sdubynin <git.user.sdubynin@inno.tech>
2025-03-02 18:20:04 +0300

    add new string

--- src/main/java/ru/lab2/project1/MainAp.java
+++ src/main/java/ru/lab2/project1/MainAp.java
@@ -4,5 +4,6 @@ public class MainAp {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
+        System.out.println("2222222222");
     }
 }
